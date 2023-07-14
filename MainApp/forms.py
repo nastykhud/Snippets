f4175d258ba213a0ae32c629ca476d7ecb64850e
nastykhud <martynasty@yandex.ru>
2023-07-14 11:14:12 +0300

    Add flag

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -6,8 +6,8 @@ class SnippetForm(ModelForm):
    class Meta:
        model = Snippet
        # Описываем поля, которые будем заполнять в форме
-       fields = ['name', 'lang', 'code']
-       labels = {'name':'Название', 'lang': '', 'code': ''} #это лейбл перед эдитом
+       fields = ['name', 'lang', 'code', 'public']
+       labels = {'name': 'Имя', 'lang': 'Язык', 'code': 'Код', 'public': 'Публичный'} #это лейбл перед эдитом
        widgets = {
            'name': TextInput(attrs = {'placeholder': 'Название сниппета'}), #это подсказки внутри эдитов
            'code': Textarea(attrs = {'placeholder': 'Код сниппета'})
